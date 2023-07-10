ddad5fcf45e2eb4fd90ec4cef7873b17eda1c0c0
briitogabriel <gabriel.brito.a@hotmail.com>
2023-07-10 20:57:52 -0300

    login page start

--- Medical365_FRONT/src/App.jsx
+++ Medical365_FRONT/src/App.jsx
@@ -1,11 +1,16 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import LoginPage from './pages/LoginPage/LoginPage'
 
 function App() {
 
   return (
-    <>
-    
-    </>
+    <Router>
+      <Routes>
+        {/* <Route path='/' element={<LoginPage/>} /> */}
+        <Route path='/login' element={<LoginPage/>} />
+      </Routes>
+    </Router>
   )
 }
 
