21e4d6e2dd6d4895fb9902b192766f54af03b4de
briitogabriel <gabriel.brito.a@hotmail.com>
2023-07-12 19:29:55 -0300

    redirect home and form component

---
+++ Medical365_FRONT/src/components/FormLoginComponent/FormLoginComponent.style.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Form = styled.form`
+  display: inline-flex;
+  padding: 2rem;
+  
+`;
+export const Header = styled.div``;
+export const InputGroup = styled.div``;
+export const Button = styled.button``;
+export const Action = styled.div``;
