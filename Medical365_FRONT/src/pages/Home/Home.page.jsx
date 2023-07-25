59799caf94fc41b136039632e2975921c339a165
briitogabriel <gabriel.brito.a@hotmail.com>
2023-07-25 12:10:18 -0300

    layout and homepage validating auth context

---
+++ Medical365_FRONT/src/pages/Home/Home.page.jsx
@@ -0,0 +1,18 @@
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/auth/auth.context"
+import { Navigate } from "react-router-dom";
+
+export const HomePage = () => {
+  const { auth } = useContext(AuthContext);
+  console.log(auth)
+
+  const render = () => {
+    return(
+      <>
+        <p>HomePage is Render</p>
+      </>
+    )
+  }
+
+  return auth.isLogged ? render() : <Navigate to='/login'/>
+}
