899b720f23f6431ad57875fb8e00776e0bc06843
briitogabriel <gabriel.brito.a@hotmail.com>
2023-07-26 20:59:44 -0300

    toolbar component and styles

--- Medical365_FRONT/src/components/ToolbarComponent/ToolbarComponent.jsx
+++ Medical365_FRONT/src/components/ToolbarComponent/ToolbarComponent.jsx
@@ -1,6 +1,20 @@
+import { useContext } from "react"
+import { AuthContext } from "../../contexts/auth/auth.context"
+import * as Styled from './ToolbarComponent.style.jsx'
 
 export default function ToolbarComponent() {
+  
+    const { auth } = useContext(AuthContext);
+    const loggedUser = auth.isLogged ? auth : JSON.parse(localStorage.getItem('logged-user'));
+    let username = loggedUser.user.email.split('@',1)
+    
   return (
-    <div>ToolbarComponent</div>
+    <Styled.Nav>
+      ESTATÍSTICAS E INFORMAÇÕES
+      <Styled.UserInfo>
+        {username}
+        <Styled.Icon />
+      </Styled.UserInfo>
+    </Styled.Nav>
   )
 }
---
+++ Medical365_FRONT/src/components/ToolbarComponent/ToolbarComponent.style.jsx
@@ -0,0 +1,24 @@
+import styled from 'styled-components'
+
+export const Icon = styled.div`
+  border-radius: 50%;
+  background-color: gray;
+  height: 40px;
+  width: 40px;
+  border: 1px solid black;
+`;
+
+export const Nav = styled.nav`
+  justify-content: space-between;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 2%;
+`;
+
+export const UserInfo = styled.div`
+  justify-content: space-around;
+  display: flex;
+  align-items: center;
+  gap: 5px;
+`;
