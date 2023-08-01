26adfa5740a769880e682016e8ba17e0f514bada
briitogabriel <gabriel.brito.a@hotmail.com>
2023-08-01 15:53:04 -0300

    scroll fix

--- Medical365_FRONT/src/pages/Home/Home.page.jsx
+++ Medical365_FRONT/src/pages/Home/Home.page.jsx
@@ -3,11 +3,51 @@ import { AuthContext } from "../../contexts/auth/auth.context"
 import { Navigate } from "react-router-dom";
 import { StatisticComponent } from "../../components/StatisticComponent/StatisticComponent";
 
+import { BiSolidUserCircle } from 'react-icons/bi';
 import * as Styled from './Home.style'
+import { PatientCardComponent } from "../../components/PatientCardComponent/PatientCardComponent";
 
 export const HomePage = () => {
   const { auth } = useContext(AuthContext);
   const loggedUser = JSON.parse(localStorage.getItem('logged-user'))
+
+  const patientData = [
+    {
+      icon: BiSolidUserCircle,
+      name: 'Cesar Abascal',
+      age: '30 anos',
+      phone: '(55) 9 9999-9999',
+      plan: 'Unimed'
+    },
+    {
+      icon: BiSolidUserCircle,
+      name: 'Cesar Abascal',
+      age: '30 anos',
+      phone: '(55) 9 9999-9999',
+      plan: 'Unimed'
+    },
+    {
+      icon: BiSolidUserCircle,
+      name: 'Cesar Abascal',
+      age: '30 anos',
+      phone: '(55) 9 9999-9999',
+      plan: 'Unimed'
+    },
+    {
+      icon: BiSolidUserCircle,
+      name: 'Cesar Abascal',
+      age: '30 anos',
+      phone: '(55) 9 9999-9999',
+      plan: 'Unimed'
+    },
+    {
+      icon: BiSolidUserCircle,
+      name: 'Cesar Abascal',
+      age: '30 anos',
+      phone: '(55) 9 9999-9999',
+      plan: 'Unimed'
+    },
+  ]
 
   const render = () => {
     return(
@@ -18,6 +58,13 @@ export const HomePage = () => {
           <StatisticComponent />
           <StatisticComponent />
         </Styled.StatisticCards>
+
+        
+        <h1>Informações Rápidas de Pacientes</h1>
+        <Styled.GridContainer>
+          <PatientCardComponent patientData={patientData} />
+        </Styled.GridContainer>
+
       </Styled.Homepage>
     )
   }
--- Medical365_FRONT/src/pages/Home/Home.style.jsx
+++ Medical365_FRONT/src/pages/Home/Home.style.jsx
@@ -9,3 +9,9 @@ export const StatisticCards = styled.div`
   display: flex;
   flex-direction: row;
 `;
+
+export const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+`;
